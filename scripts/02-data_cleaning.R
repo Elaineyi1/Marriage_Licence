55198dcb809442a309fa8c8c4e696e4cf8d681b1
Elaineyi1 <boxuan.yi@mail.utoronto.ca>
2024-09-19 21:57:02 +0000

    modify the preamble

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Clean the data from Open Data Toronto
+# Author: Boxuan Yi
+# Date: 19 September 2024
+# Contact: boxuan.yi@utoronto.ca
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(tidyverse)
